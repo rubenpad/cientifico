5c2a4b16496cd11e0e13cbfeaa7e95118d38d371
Ruben Padilla <rubbenpad2@gmail.com>
2020-02-12 18:14:24 -0500

    home template

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+function Home() {
+  const view = `
+    <div className="characters">
+      <article className="characters__item">
+        <a href="#/1/">
+          <img src="image alt="name"/>
+          <h3>Character Name</h3>
+        </a>
+      </article>
+    </div>
+  `
+
+  return view
+}
+
+export default Home
